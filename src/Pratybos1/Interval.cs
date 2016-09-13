6a5d7949409bdf3c443f1ae819933f92410f9f8f
vytautas.mackonis <vytautas.mackonis@gmail.com>
2016-09-14 00:44:20 +0300

    pirmosios pratybos

---
+++ src/Pratybos1/Interval.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratybos1
+{
+    class Interval
+    {
+        private readonly DateTime _from;
+        private readonly DateTime _to;
+
+        public Interval(DateTime from, DateTime to)
+        {
+            _from = from;
+            _to = to;
+        }
+
+        public bool Contains(DateTime value)
+        {
+            return _from <= value && _to >= value;
+        }
+
+        //public bool Overlaps(Interval another)
+        //{
+        //
+        //}
+    }
+}
---
+++ src/Pratybos1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratybos1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var interval = new Interval(new DateTime(2016, 01, 02), new DateTime(2016, 01, 05));
+
+            var line = Console.ReadLine();
+            var date = DateTime.Parse(line);
+            var result = interval.Contains(date);
+
+            Console.WriteLine(result);
+        }
+    }
+}
