7a36c5f82565768ec8bbda69c79665bcee86d3e7
vytautas.mackonis <vytautas.mackonis@gmail.com>
2016-09-15 00:00:25 +0300

    class vs struct, operator overloading, ToString()

--- src/Pratybos2/Point.cs
+++ src/Pratybos2/Point.cs
@@ -35,6 +35,24 @@ namespace Pratybos2
             X = x;
             Y = y;
         }
+
+        public static double operator -(PointClass l, PointClass r)
+        {
+            var diffX = Math.Abs(l.X - r.X);
+            var diffY = Math.Abs(l.Y - r.Y);
+
+            return Math.Sqrt(diffX * diffX + diffY * diffY);
+        }
+
+        public override string ToString()
+        {
+            return $"{X}:{Y}";
+        }
+
+        public static explicit operator string(PointClass p)
+        {
+            return p.ToString();
+        }
     }
 
     public class PointOperations
---
+++ src/Pratybos2/PointTests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Pratybos2
+{
+    public class PointTests
+    {
+        [Fact]
+        public void XCanBeAssignedOnClass()
+        {
+            var point = new PointClass(1, 2);
+            point.X = 3;
+
+            Assert.Equal(3, point.X);
+        }
+
+        [Fact]
+        public void XCanBeAssignedOnClassThroughMethod()
+        {
+            var point = new PointClass(1, 2);
+            PointOperations.SetXOnClass(point, 3);
+
+            Assert.Equal(3, point.X);
+        }
+
+        [Fact]
+        public void XCanBeAssignedOnStructThroughMethod()
+        {
+            var point = new PointStruct(1, 2);
+            PointOperations.SetXOnStruct(point, 3);
+
+            Assert.Equal(1, point.X);
+        }
+
+        [Fact]
+        public void XCanBeAssignedOnStructThroughMethodThatReturns()
+        {
+            var point = new PointStruct(1, 2);
+            point = PointOperations.SetXOnStruct(point, 3);
+
+            Assert.Equal(3, point.X);
+        }
+
+        [Fact]
+        public void XCanBeAssignedOnStructThroughInterface()
+        {
+            IPoint point = new PointStruct(1, 2);
+            PointOperations.SetXOnInterface(point, 3);
+
+            Assert.Equal(3, point.X);
+        }
+
+        [Fact]
+        public void DistanceCanBeCalculated()
+        {
+            var p1 = new PointClass(1, 0);
+            var p2 = new PointClass(4, 0);
+
+            var distance = p2 - p1;
+            Assert.Equal(3.0, distance);
+        }
+
+        [Fact]
+        public void ToStringWorksCorrectly()
+        {
+            var p = new PointClass(1, 4);
+            var message = "p: " + p;
+            Assert.Equal("p: 1:4", message);
+        }
+
+        [Fact]
+        public void ConversionWorksCorrectly()
+        {
+            string p = (string)new PointClass(1, 4);
+            var message = "p: " + p;
+            Assert.Equal("p: 1:4", message);
+        }
+    }
+}
