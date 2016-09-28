83f6338b8926ec70c52b6b25da95d031c757323e
vytautas.mackonis <vytautas.mackonis@gmail.com>
2016-09-28 15:43:50 +0300

    pasiruosimas 4 pratyboms

---
+++ src/Pratybos4/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratybos4
+{
+    class Person
+    {
+        public long SocialSecurityNumber { get;}
+        public string FirstName { get;}
+        public string LastName { get; }
+
+        public Person(long socialSecurityNo, string firstName, string lastName)
+        {
+            SocialSecurityNumber = socialSecurityNo;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
