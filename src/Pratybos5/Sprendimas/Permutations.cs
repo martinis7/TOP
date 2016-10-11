48d8bbbd5fe0136607f843cfc2fab6ff3d94eafa
vytautas.mackonis <vytautas.mackonis@gmail.com>
2016-10-12 01:43:37 +0300

    kombinaciju sprendimai

---
+++ src/Pratybos5/Sprendimas/Permutations.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratybos5.Sprendimas
+{
+    class Permutations
+    {
+        private static List<T[]> PermutationsOf<T>(T[] array, int level)
+        {
+            if (level == array.Length - 1)
+                return new List<T[]>
+                {
+                    array
+                };
+
+            var permutations = new List<T[]>();
+            permutations.AddRange(PermutationsOf(array, level + 1));
+
+            for (var otherElementIndex = level + 1; otherElementIndex < array.Length; otherElementIndex++)
+            {
+                var copy = array.ToArray();
+                Swap(copy, level, otherElementIndex);
+                permutations.AddRange(PermutationsOf(copy, level + 1));
+            }
+
+            return permutations;
+        }
+
+        public static IEnumerable<IEnumerable<T>> Recursive<T>(T[] array)
+        {
+            return PermutationsOf(array, 0);
+        }
+
+        private static List<int[]> IntPermutations(int length)
+        {
+            var permutations = new List<int[]>();
+            var permutation = Enumerable.Range(0, length).ToArray();
+
+            do
+            {
+                permutations.Add(permutation);
+                permutation = NextIntPermutation(permutation);
+            }
+            while (permutation != null);
+
+            return permutations;
+        }
+
+        private static T[] MakePermutation<T>(T[] array, int[] intPermutation)
+        {
+            var result = new T[array.Length];
+
+            for (var i = 0; i < array.Length; i++)
+            {
+                result[i] = array[intPermutation[i]];
+            }
+
+            return result;
+        }
+
+        public static IEnumerable<IEnumerable<T>> Iterative<T>(T[] array)
+        {
+            var intPermutations = IntPermutations(array.Length);
+
+            var permutations = new List<IEnumerable<T>>();
+
+            foreach (var intPermutation in intPermutations)
+            {
+                permutations.Add(MakePermutation(array, intPermutation));
+            }
+
+            return permutations;
+        }
+
+        private static int FindOutOfOrderIndex(int[] array)
+        {
+            for (var i = array.Length - 2; i >= 0; i--)
+            {
+                if (array[i] < array[i + 1])
+                    return i;
+            }
+
+            return -1;
+        }
+
+        private static int FindLargerThan(int[] array, int value)
+        {
+            for (var i = array.Length - 1; i >= 0; i--)
+            {
+                if (array[i] > value)
+                    return i;
+            }
+
+            return -1;
+        }
+
+        private static int[] NextIntPermutation(int[] prev)
+        {
+            var outOfOrderIndex = FindOutOfOrderIndex(prev);
+            if (outOfOrderIndex == -1) return null;
+
+            var largerIndex = FindLargerThan(prev, prev[outOfOrderIndex]);
+            var next = prev.ToArray();
+            Swap(next, outOfOrderIndex, largerIndex);
+
+            Array.Reverse(next, outOfOrderIndex + 1, next.Length - outOfOrderIndex - 1);
+            return next;
+        }
+
+        private static void Swap<T>(T[] array, int idx1, int idx2)
+        {
+            var tmp = array[idx1];
+            array[idx1] = array[idx2];
+            array[idx2] = tmp;
+        }
+    }
+}
---
+++ src/Pratybos5/Sprendimas/PermutationsTests.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Pratybos5.Sprendimas
+{
+    public abstract class PermutationsTests
+    {
+        [Theory]
+        [InlineData("a")]
+        [InlineData("b")]
+        public void AllCombinationsOfASingleElementIsTheSameElement(string element)
+        {
+            var expected = new[]
+            {
+                new [] { element }
+            };
+            var actual = PermutationsOf(new[] { element });
+            Assert.Equal(expected, actual, CombinationsComparer<string>());
+        }
+
+        [Theory]
+        [InlineData(new[] { 1, 2 })]
+        [InlineData(new[] { 7, 12 })]
+        [InlineData(new[] { 7, 7 })]
+        public void AllCombinationsOfTwoElementsSequenceIsSameSequenceAndReversedSequence(int[] sequence)
+        {
+            var expected = new[]
+            {
+                sequence,
+                sequence.Reverse().ToArray()
+            };
+
+            var actual = PermutationsOf(sequence);
+            Assert.Equal(expected, actual, CombinationsComparer<int>());
+        }
+
+        [Fact]
+        public void AllCombinationsOfThreeElementSequenceAreReturned()
+        {
+            var sequence = new[] { "a", "b", "c" };
+
+            var expected = new[]
+            {
+                new[] { "a", "b", "c" },
+                new[] { "a", "c", "b" },
+                new[] { "b", "a", "c" },
+                new[] { "b", "c", "a" },
+                new[] { "c", "b", "a" },
+                new[] { "c", "a", "b" },
+            };
+
+            var actual = PermutationsOf(sequence);
+            Assert.Equal(expected, actual, CombinationsComparer<string>());
+        }
+
+        [Fact]
+        public void AllCombinationsOfFourElementSequenceAreReturned()
+        {
+            var sequence = new[] { 1, 7, 4, 12 };
+
+            var expected = new[]
+            {
+                new[] { 1, 7, 4, 12 },
+                new[] { 1, 7, 12, 4 },
+                new[] { 1, 4, 7, 12 },
+                new[] { 1, 4, 12, 7 },
+                new[] { 1, 12, 4, 7 },
+                new[] { 1, 12, 7, 4 },
+
+                new[] { 7, 1, 4, 12 },
+                new[] { 7, 1, 12, 4 },
+                new[] { 7, 4, 1, 12 },
+                new[] { 7, 4, 12, 1 },
+                new[] { 7, 12, 1, 4 },
+                new[] { 7, 12, 4, 1 },
+
+                new[] { 4, 1, 7, 12 },
+                new[] { 4, 1, 12, 7 },
+                new[] { 4, 7, 1, 12 },
+                new[] { 4, 7, 12, 1 },
+                new[] { 4, 12, 1, 7 },
+                new[] { 4, 12, 7, 1 },
+
+                new[] { 12, 1, 7, 4 },
+                new[] { 12, 1, 4, 7 },
+                new[] { 12, 7, 1, 4 },
+                new[] { 12, 7, 4, 1 },
+                new[] { 12, 4, 1, 7 },
+                new[] { 12, 4, 7, 1 }
+            };
+
+            var actual = PermutationsOf(sequence);
+            Assert.Equal(expected, actual, CombinationsComparer<int>());
+        }
+
+        protected abstract IEnumerable<IEnumerable<T>> PermutationsOf<T>(T[] sequence);
+
+        private static IEqualityComparer<IEnumerable<IEnumerable<T>>> CombinationsComparer<T>()
+        {
+            return new SetEqualityComparer<IEnumerable<T>>(new SequenceEqualityComparer<T>());
+        }
+
+
+        class SequenceEqualityComparer<T> : IEqualityComparer<IEnumerable<T>>
+        {
+            public bool Equals(IEnumerable<T> x, IEnumerable<T> y)
+            {
+                return x.SequenceEqual(y);
+            }
+
+            public int GetHashCode(IEnumerable<T> obj)
+            {
+                return 1;
+            }
+        }
+
+        class SetEqualityComparer<T> : IEqualityComparer<IEnumerable<T>>
+        {
+            private readonly IEqualityComparer<T> _elementComparer;
+
+            public SetEqualityComparer(IEqualityComparer<T> elementComparer)
+            {
+                _elementComparer = elementComparer;
+            }
+
+            public bool Equals(IEnumerable<T> x, IEnumerable<T> y)
+            {
+                var xArray = x.ToArray();
+                var yArray = y.ToArray();
+                if (xArray.Length != yArray.Length)
+                    return false;
+
+                foreach (var element in xArray)
+                {
+                    if (!yArray.Contains(element, _elementComparer))
+                        return false;
+                }
+
+                return true;
+            }
+
+            public int GetHashCode(IEnumerable<T> obj)
+            {
+                return 1;
+            }
+        }
+    }
+
+    public class RecursivePermutationsTests : PermutationsTests
+    {
+        protected override IEnumerable<IEnumerable<T>> PermutationsOf<T>(T[] sequence)
+        {
+            return Permutations.Recursive(sequence);
+        }
+    }
+
+    public class IterativePermutationsTests : PermutationsTests
+    {
+        protected override IEnumerable<IEnumerable<T>> PermutationsOf<T>(T[] sequence)
+        {
+            return Permutations.Iterative(sequence);
+        }
+    }
+}
